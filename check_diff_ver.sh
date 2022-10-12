97691e27afd1ffc15a866f0c158d7b216840aead
DPDmancul <davide.peressoni@tuta.io>
2022-10-12 18:25:54 +0200

    Added CI utilities

---
+++ check_diff_ver.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Check if there is a difference in the given file for version from last tag to current tag returns true if difference is found.
+# If this is the first tag returns false.
+
+this_tag="$(git tag --sort version:refname | tail -n 1)"
+last_tag="$(git tag --sort version:refname | tail -n 2 | head -n 1)"
+
+[ "$last_tag" = "" ] && exit 0 # no last tag => return true
+
+# find version in diff, return true if found
+git diff "$last_tag" "$this_tag" -- "$1" | grep version
