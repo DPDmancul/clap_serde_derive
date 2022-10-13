638eeb3e707ba5bb5bce3c6ceed1fe46dcdda828
DPDmancul <davide.peressoni@tuta.io>
2022-10-13 18:12:02 +0200

    Fixed error

--- check_diff_ver.sh
+++ check_diff_ver.sh
@@ -6,7 +6,7 @@
 this_tag="$(git tag --sort version:refname | tail -n 1)"
 last_tag="$(git tag --sort version:refname | tail -n 2 | head -n 1)"
 
-[ "$last_tag" = "" ] && exit 0 # no last tag => return true
+[ "$last_tag" = "" ] || [ "$last_tag" = "$this_tag" ] && exit 0 # no last tag => return true
 
 # find version in diff, return true if found
 git diff "$last_tag" "$this_tag" -- "$1" | grep version
